9ee959015dc157f4d44ce017945257e9a13c6ff5
navya-shine <69377970+navya-shine@users.noreply.github.com>
2020-12-16 16:52:35 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -14,7 +14,7 @@ document.getElementById("player_question").innerHTML = "Question Turn -" + playe
 document.getElementById("player_answer").innerHTML = "Answer Turn -" + player2_name ;
 
 function send() {
-    get_word + document.getElementById("word").value;
+    get_word = document.getElementById("word").value;
     word = get_word.toLowerCase();
     console.log("word in lowerCase = " + word);
 
@@ -42,5 +42,50 @@ function send() {
     
     document.getElementById("output").innerHTML = row;
     document.getElementById("word").value = "";
+
+}
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lower case - " + answer);
+    if(answer == word )
+    {
+        if(answer_turn == "player1" )
+        {
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else
+        {
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+    if(question_turn == "player1" )
+    {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else 
+    {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+    if(answer_turn == "player1" )
+    {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else 
+    {
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "nswer Turn - " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
 
 }
